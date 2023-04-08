bc197d0d291085f9823b0c6f0868b9f6a8b23d55
Mario <mario1999345@gmail.com>
2023-04-08 14:59:11 -0600

    Añadiendo hojas de estilos CSS

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: "app-contador",
     templateUrl:"./counter.component.html",
+    styleUrls:["./counter.component.css"],
 })
 export class CounterComponent{
     title="Contador";
